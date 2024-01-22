3ddd340b0fe359359de367b7532a03de54d731eb
rahul72pal <110774048+rahul72pal@users.noreply.github.com>
2024-01-22 12:02:27 +0000

    ok

--- config/razorpay.js
+++ config/razorpay.js
@@ -1,4 +1,5 @@
 const razorpay = require("razorpay");
+console.log(process.env.RAZORPAY_KEY);
 const instance = new razorpay({
   key_id: process.env.RAZORPAY_KEY,
   key_secret: process.env.RAZORPAY_SECRET,
